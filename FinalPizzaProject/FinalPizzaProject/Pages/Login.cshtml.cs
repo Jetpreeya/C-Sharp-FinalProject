4ccf4d380bf0b7a821922461b092a4c9742fa398
Jetpreeya <jetpreeya.norkaew@gmail.com>
2022-07-04 11:12:24 +0200

    Login class prop and style with Div

--- FinalPizzaProject/FinalPizzaProject/Pages/Login.cshtml.cs
+++ FinalPizzaProject/FinalPizzaProject/Pages/Login.cshtml.cs
@@ -1,12 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace FinalPizzaProject.Pages
 {
     public class LoginModel : PageModel
     {
+        [BindProperty]
+        public Credential Credential { get; set; }
+       
+
         public void OnGet()
         {
         }
+    }
+
+    public class Credential
+    {
+        [Required]
+        public String UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public String Password { get; set; }    
     }
 }
